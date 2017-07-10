fc85f8ba7b41b0722828cb267c2a7f8c2f562190
Botoro <boris_parra@hotmail.com>
2017-07-10 17:30:51 -0400

    con bloque

--- game.js
+++ game.js
@@ -5,9 +5,33 @@ ctx.canvas.height = 500;
 
 const cWidth = ctx.canvas.width;
 const cHeight = ctx.canvas.height;
+
+class Player{
+	
+	constructor(posX=0, posY=0, width=0, height=0){
+		this.posX = posX;
+		this.posY = posY;
+		this.width = width;
+		this.height = height;
+	}
+	//player =  {posX: (ctx.canvas.width/2)-30, posY: ctx.canvas.height-10, width: 100, height: 10};
+
+}
+
+class Bloque {
+	constructor(posX=0, posY=0, width=0, height=0, estado=0){
+		this.posX = posX;
+		this.posY = posY;
+		this.width = width;
+		this.height = height;
+		this.estado = estado;
+	}
+}
+
 var velocidad = 6;
 var game_loop;
 var player;
+var bloque;
 var start_loop;
 var s;
 var vidas;
@@ -30,6 +54,10 @@ playerSprite.src = "nave.jpg";
 
 document.getElementById("canvas").addEventListener("click", function(){
 
+	if(!playerVivo){
+		init();
+	}
+
 	if(clic==0 ){
 		keyP = "r";
 		clic=1;
@@ -41,27 +69,10 @@ document.getElementById("canvas").addEventListener("click", function(){
 
 
 },false);
+
 document.addEventListener("keydown",function(event){
-	console.log("ahora si"+event.which);
-},false);
-
-/*
-//Captura que tecla se presiono
-$(document).keydown(function(event){
-event.preventDefault();
-var key = event.which;
-switch(key){
-		case (87):
-		case (38):
-			if (velocidad < 13)
-				velocidad +=1;
-			break;
-		case 83:
-		case (40):
-			if (velocidad > 5)
-				velocidad -=1;
-			break;
-
+	var key = event.which;
+	switch(key){
 		case 68:
 		case (39):
 			keyP = "r";
@@ -71,59 +82,61 @@ switch(key){
 			keyP = "l";
 			break;
 		}
-if (key==13){
-	init();
-}
+		if (key==13 && !playerVivo){
+			init();
+		}
 
-})
-
-//Captura que tecla se presiono
-$(document).keyup(function(event){
-})*/
-
+},false);
 
 function init(){
-s=0; vidas=3;
-player =  {posX: (ctx.canvas.width/2)-30, posY: ctx.canvas.height-20, width: 100, height: 20};
-pelota = {posX: (ctx.canvas.width/2), 
-		  posY: 10, dirX: 5, dirY:10, width:10 , height:10 };
+	s=0; vidas=3;
+	//player =  {posX: (ctx.canvas.width/2)-30, posY: ctx.canvas.height-10, width: 100, height: 10};
 
-if(typeof game_loop != "undefined"){
-	clearInterval(game_loop);
-}
-clearInterval(start_loop);
-game_loop = setInterval(main, 30);
-stopPlayer = false;
-stopEnemigo = false;
-keyP="";
+	player = new Player((ctx.canvas.width/2)-30,ctx.canvas.height-10,100,10);
 
+	pelota = {posX: (ctx.canvas.width/2), posY: 10, dirX: 5, dirY:10, per:7};
 
+	bloque = new Bloque(10,300, 80, 10, 1);
+
+	if(typeof game_loop != "undefined"){
+		clearInterval(game_loop);
+	}
+	clearInterval(start_loop);
+	game_loop = setInterval(main, 30);
+	stopPlayer = false;
+	stopEnemigo = false;
+	playerVivo = true;
+	keyP="";
 }
 
 function getRandom(min, max) {
-return Math.floor(Math.random() * (max - min)) + min;
+	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function main(){
-drawBackground();
-score();
-vidasTexto();
-if (playerVivo){
-	drawPlayer();
-	drawPelota();
-	rebotePelota();
-	colision();	
-}else{
-	gameOver();
-}
+	drawBackground();
 
-if (!stopPlayer)		
-	movePlayer(keyP);				
+	if (playerVivo){
+		drawPlayer();
+		drawPelota();
+		if (bloque.estado==1){
+			drawBloques();
+		}
+		rebotePelota();
+		colision();	
+	}else{
+		gameOver();
+	}
+
+	if (!stopPlayer)		
+		movePlayer(keyP);	
+	score();
+	vidasTexto();			
 }
 
 function drawBackground(){
-ctx.save();
-ctx.fillStyle = "black";
+	ctx.save();
+	ctx.fillStyle = "#000";
 	ctx.fillRect (0,0,cWidth,cHeight);
 	ctx.strokeStyle = "red"; 
 	ctx.strokeRect (0,0,cWidth,cHeight);
@@ -132,38 +145,44 @@ ctx.fillStyle = "black";
 
 }
 function drawPlayer(){
-ctx.save();
-ctx.fillStyle = "white";
-ctx.fillRect (player.posX, player.posY, player.width, player.height);
-//ctx.drawImage(playerSprite,player.posX,player.posY,player.width,player.height);
-ctx.restore();
+	ctx.save();
+	ctx.fillStyle = "#FFF";
+	ctx.fillRect (player.posX, player.posY, player.width, player.height);
+	//ctx.drawImage(playerSprite,player.posX,player.posY,player.width,player.height);
+	ctx.restore();
 }
 
 function drawPelota(){
-ctx.save()
-ctx.beginPath();
-// ctx.clearRect(0,0,700,500);
-ctx.fillStyle="yellow";
-ctx.arc(pelota.posX, pelota.posY, 10, 0, 360,false);
-ctx.fill();
-ctx.restore();
+	ctx.save()
+	ctx.beginPath();
+	// ctx.clearRect(0,0,700,500);
+	ctx.fillStyle="yellow";
+	ctx.arc(pelota.posX, pelota.posY, pelota.per, 0, 360,false);
+	ctx.fill();
+	ctx.restore();
+}
 
+function drawBloques(){
+	if (bloque.estado==1){
+		ctx.save();
+		ctx.beginPath();
+		ctx.fillStyle="red";
+		ctx.fillRect(bloque.posX, bloque.posY, bloque.width, bloque.height);
+		ctx.restore();
+	}
 }
 
 		
 // funciones del jugador
 function movePlayer(dir){
-
-switch(dir){
-		case "r":
-			player.posX += +velocidad;
-			break;
-		case "l":
-			player.posX += -velocidad;
-			break;
-		}
-
-
+	switch(dir){
+	case "r":
+		player.posX += +velocidad;
+		break;
+	case "l":
+		player.posX += -velocidad;
+		break;
+	}
 }
 
 
@@ -174,16 +193,15 @@ function colision(){
 		player.posX = 0;
 	}
 
-	if(player.posX > (cWidth-player.width)){
+	if(player.posX >= (cWidth-player.width)){
 		player.posX = cWidth-player.width;
 	}
 
-	if (player.posY == pelota.posY+pelota.height
-		&& pelota.posX >= player.posX-15 
-		&& pelota.posX <= player.posX+15+player.width){
-
+	if (player.posY <= pelota.posY+pelota.per
+		&& pelota.posX >= player.posX 
+		&& pelota.posX <= player.posX+player.width){
 		//cambia dirección de pelota
-		if (pelota.posX < player.posX+(player.width/2)){
+		if (pelota.posX <= player.posX+(player.width/2)){
 			cambioDireccion('up');
 			cambioDireccion('left');
 		}
@@ -197,6 +215,21 @@ function colision(){
 		destruyePelota();
 	}
 
+	//bloques colision lo hare por parte para entender.
+	if (pelota.posX >= bloque.posX && pelota.posX <= bloque.posX + bloque.width &&
+		pelota.posY >= bloque.posY && pelota.posY <= bloque.posY + bloque.height){
+		
+		if (pelota.posX <= bloque.posX+(player.width/2)){
+			cambioDireccion('left');
+		}
+		else{
+			cambioDireccion('right');
+		}
+		console.log("pelota 0");
+		bloque.estado = 0;
+
+	}
+
 
 }
 
@@ -204,7 +237,7 @@ function destruyePelota(){
 	pelota.posX = (ctx.canvas.width/2);
 	pelota.posY = 10;
 	vidas--;
-	player.width -= 30;
+	//player.width -= 30;
 	if(vidas<=0){
 		gameOver();
 		playerVivo=false;
@@ -212,18 +245,15 @@ function destruyePelota(){
 }
 
 function rebotePelota(){
-	if(pelota.posY == pelota.height){
+	if(pelota.posY <= pelota.per){
 		cambioDireccion('down');
 	}
 
-	if (pelota.posY == ctx.canvas.height){
-		cambioDireccion('up');
-	}
-
-	if(pelota.posX == ctx.canvas.width){
+	if(pelota.posX >= ctx.canvas.width){
 		cambioDireccion('left');
 	}
-	if(pelota.posX == pelota.height){
+
+	if(pelota.posX <= pelota.per){
 		cambioDireccion('right');
 	}
 
@@ -258,6 +288,9 @@ function gameOver(){
 	ctx.fillStyle = "red";
 	ctx.textAlign = "center";
 	ctx.fillText(title,textPosX,textPosY);
+	var subtitle = "Volver a comenzar";
+	ctx.font = "bold 10px Courier";
+	ctx.fillText(subtitle,textPosX,textPosY+10)
 	ctx.restore();  
 }
 
@@ -267,7 +300,7 @@ function score(){
 	ctx.font = "bold 10px Courier";
 	ctx.fillStyle = "white";
 	ctx.textAlign = "left";
-	ctx.fillText(t,10,20);
+	ctx.fillText(t,10,10);
 	ctx.restore();			
 }
 
@@ -277,7 +310,7 @@ function vidasTexto(){
 	ctx.font = "bold 10px Courier";
 	ctx.fillStyle = "white";
 	ctx.textAlign = "left";
-	ctx.fillText(t,cWidth-60,20);
+	ctx.fillText(t,cWidth-60,10);
 	ctx.restore();			
 }
 
